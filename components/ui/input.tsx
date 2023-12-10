6afd872a50424250cead4e70b7d40159ef824228
Prajwal Joshi <prajwaljoshi@Prajwals-MacBook-Pro.local>
2023-12-10 20:04:13 +0545

    fix: re,  contact form input borders on sm devices

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-16 w-80 md:w-full rounded-lg border  border-black bg-background px-4 py-2 text-md font-light tracking-wide text-black   file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 placeholder:font-light placeholder:tracking-wide  focus:border-2 focus:border-[#344A80] focus-visible:border-[#344A80] disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-16 w-80 md:w-full rounded-lg border  border-black bg-background px-4 py-2 text-md font-light tracking-wide text-black   file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 placeholder:font-light placeholder:tracking-wide  focus-visible:border-[#344A80] disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
