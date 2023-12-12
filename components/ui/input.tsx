46891dc53ffd2522a0b3dea2c60980644cd2d2aa
Prajwal Joshi <prajwaljoshi@Prajwals-MacBook-Pro.local>
2023-12-12 17:38:41 +0545

    style: changes full layout of contact and responsive

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-14 w-[325px] md:w-full rounded-md border border-black  bg-background px-4 py-2 text-md font-light  file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground placeholder:font-light focus-visible:outline-none focus-visible:border-2 focus-visible:border-border focus-visible:border-[#344A80] disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-[325px] md:w-full  border border-black  bg-background px-4 py-2 text-md font-light  file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground placeholder:font-light focus-visible:outline-none focus-visible:border-2 focus-visible:border-border focus-visible:border-[#344A80] disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
