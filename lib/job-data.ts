8aa2ec55d79cf474ecd5bcd0112b793829726f58
Prajwal Joshi <prajwaljoshi@Prajwals-MacBook-Pro.local>
2023-12-13 15:39:38 +0545

    build: career individual page full responsive

---
+++ lib/job-data.ts
@@ -0,0 +1,75 @@
+export const jobData = [
+  {
+    post: "Senior Full Stack Developer",
+    location: "Remote",
+    responsibilities: [
+      "Develop and maintain high-quality software solutions.",
+      "Collaborate with cross-functional teams to define, design, and ship new features.",
+      "Troubleshoot, debug, and optimize code for performance.",
+      "Proficient in programming languages such as Python, JavaScript, or Java.",
+      "Experience with front-end frameworks like React or Angular.",
+      "Strong problem-solving and debugging skills.",
+      "Excellent communication and teamwork abilities."
+    ],
+    preferred: [
+      "3+ years of experience in software development.",
+      "Experience with web application development.",
+      "Previous work on projects involving [specific technology/domain].",
+    ],
+
+  },
+  {
+    post: "Senior Frontend Developer",
+    location: "Remote",
+    responsibilities: [
+      "Develop and maintain high-quality software solutions.",
+      "Collaborate with cross-functional teams to define, design, and ship new features.",
+      "Troubleshoot, debug, and optimize code for performance.",
+      "Proficient in programming languages such as Python, JavaScript, or Java.",
+      "Experience with front-end frameworks like React or Angular.",
+      "Strong problem-solving and debugging skills.",
+      "Excellent communication and teamwork abilities."
+    ],
+    preferred: [
+      "3+ years of experience in software development.",
+      "Experience with web application development.",
+      "Previous work on projects involving [specific technology/domain].",
+    ],
+  },
+  {
+    post: "Mobile App Developer",
+    location: "Remote",
+    responsibilities: [
+      "Develop and maintain high-quality software solutions.",
+      "Collaborate with cross-functional teams to define, design, and ship new features.",
+      "Troubleshoot, debug, and optimize code for performance.",
+      "Proficient in programming languages such as Python, JavaScript, or Java.",
+      "Experience with front-end frameworks like React or Angular.",
+      "Strong problem-solving and debugging skills.",
+      "Excellent communication and teamwork abilities."
+    ],
+    preferred: [
+      "3+ years of experience in software development.",
+      "Experience with web application development.",
+      "Previous work on projects involving [specific technology/domain].",
+    ],
+  },
+  {
+    post: "Mobile App Developer",
+    location: "Remote",
+    responsibilities: [
+      "Develop and maintain high-quality software solutions.",
+      "Collaborate with cross-functional teams to define, design, and ship new features.",
+      "Troubleshoot, debug, and optimize code for performance.",
+      "Proficient in programming languages such as Python, JavaScript, or Java.",
+      "Experience with front-end frameworks like React or Angular.",
+      "Strong problem-solving and debugging skills.",
+      "Excellent communication and teamwork abilities."
+    ],
+    preferred: [
+      "3+ years of experience in software development.",
+      "Experience with web application development.",
+      "Previous work on projects involving [specific technology/domain].",
+    ],
+  },
+];
